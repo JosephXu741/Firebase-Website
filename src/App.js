9e7272d01a3454f9d77e5012f226170bb9cf2635
Joseph Xu <joseph.xu@student.unsw.edu.au>
2021-05-27 01:44:43 +1000

    added initial version of React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Landing from "./pages/Landing.js"
+import About from "./pages/About.js"
 
 function App() {
   return (
-      <Landing />
+    <Router>
+      <Switch>
+
+        <Route path="/about">
+          <About/>
+        </Route>
+
+        <Route path="/">
+          <Landing />
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {AppBar, Toolbar, IconButton} from '@material-ui/core';
+import {NavLink} from "react-router-dom";
 
 function Navbar(props) {
     return (
@@ -7,13 +8,19 @@ function Navbar(props) {
             <AppBar position="fixed">
                 <Toolbar variant="dense">
                     <IconButton edge="start" >
-                        Home
+                        <NavLink to="/">
+                            Home
+                        </NavLink>
                     </IconButton>
                     <IconButton edge="start" >
-                        About
+                        <NavLink to="/about">
+                            About
+                        </NavLink>
                     </IconButton>
                     <IconButton edge="end" >
-                        Login
+                        <NavLink to="/">
+                            Login
+                        </NavLink>
                     </IconButton>
                 </Toolbar>
             </AppBar>
