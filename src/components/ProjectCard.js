98f3bc8714d0a3268ac9c7599599ce2cd78c57cc
JosephXu741 <joseph499@hotmail.com>
2021-09-05 03:12:34 +1000

    project title and content layouts

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,14 +4,22 @@ import "../styles/ProjectCard.css"
 function ProjectCard(props) {
     const {alternate} = props
     return (
-        <div className={`bg-green-100 p-card ${alternate ? "justify-end" : ""}`} >
+        <div className={`p-card ${alternate ? "justify-end" : ""}`} >
             <div className="display bg-purple-200">
 
             </div>
-            <div className=" bg-green-300 p-textbox">
+            <div className="p-textbox">
 
-                <div className="p-text bg-green-500">
-                    Project card
+                <div className="p-text">
+                    <div className=" text-6xl mb-12" > 
+                        Project 01
+                    </div>
+                    <div className=" hidden sm:grid text-base md:text-lg md:mb-8" >
+                        Created by a group of passionate students, Project 01 is the
+                        amalgamation of ideas and innovations new to the technology world. 
+                        It aims to break boundaries and aspiries to continually
+                        transform what we believe is the ground truth
+                    </div>
                     
                     <div className="arrow bg-blue-600">
 
